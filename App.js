63f1b0b21b9c4f8b017b389ef6e5728e84479e34
Adam Gorring <adamgorring@gmail.com>
2024-01-03 14:52:35 +1000

    Implemented useState

--- App.js
+++ App.js
@@ -1,7 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as yup from "yup"
+
+const PasswordSchema = yup.object().shape({
+  passwordLength: yup.number().min(4, "Should be a min of 4 characters").max(16, "Should be a max of 16 characters").required("Length is required")
+})
 
 export default function App() {
+
+  const [password, setPassword] = useState("")
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, useNumbers] = useState(false)
+  const [symbols, useSymbols] = useState(false)
+
+  const generatePasswordString = (passwordLength) => {
+
+
+  }
+
+  const createPassword = (characters, passwordLength) => {
+
+  }
+
+  const resetPasswordState = () => {
+    
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
