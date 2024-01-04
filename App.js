5e155cf0f3e7ad6cf858992cce6969ed6290121e
Adam Gorring <adamgorring@gmail.com>
2024-01-04 11:29:39 +1000

    Add further styling, remove redundant code

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View, TextInput, Image} from 'react-native';
-import * as yup from "yup"
-import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import Slider from "@react-native-community/slider";
 import { Formik } from 'formik';
-import {useState} from "react"
-import Slider from "@react-native-community/slider"
+import { useState } from "react";
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import * as yup from "yup";
 
 const PasswordSchema = yup.object().shape({
   passwordLength: yup.number().min(4, "Should be a min of 4 characters").max(16, "Should be a max of 16 characters").required("Length is required")
@@ -18,7 +18,6 @@ export default function App() {
   const [upperCase, setUpperCase] = useState(false)
   const [numbers, setNumbers] = useState(false)
   const [symbols, setSymbols] = useState(false)
-  const [sliderValue, setSliderValue] = useState(10)
 
   const generatePasswordString = (passwordLength) => {
     let characterList = ""
@@ -83,80 +82,80 @@ export default function App() {
           </View>
         <View style={styles.formContainer}>
           <Formik
-      initialValues={{ passwordLength: 10 }}
-        validationSchema={PasswordSchema}
-        onSubmit={ values => {
-          generatePasswordString(Number(values.passwordLength))
-        }}
-    >
-      {({
-        values,
-        isValid,
-        handleChange,
-        handleSubmit,
-        handleReset
-      }) => (
-        <>
-        <View style={styles.sliderValueContainer}>
-            <Text style={styles.sliderValueText}>Length</Text>
-            <Text style={styles.sliderValueText}>{values.passwordLength}</Text>
-          </View>
-          <View style={styles.slider}>
-          <Slider
-            style={{ width: 300, height: 40}}
-            step={1}
-            minimumValue={4}
-            maximumValue={16}
-            value={values.passwordLength}  // Update the value from Formik values
-            onValueChange={(value) => {
-              handleChange("passwordLength")(String(value));  // Update Formik values
+          initialValues={{ passwordLength: 10 }}
+            validationSchema={PasswordSchema}
+            onSubmit={ values => {
+              generatePasswordString(Number(values.passwordLength))
             }}
-            minimumTrackTintColor='#7676ff'
-          />
-        </View>
-        <View style={styles.inputWrapper}>
-          <BouncyCheckbox 
-            disableBuiltInState 
-            isChecked={lowerCase} 
-            onPress={() => setLowerCase(!lowerCase)} 
-            fillColor='#7676ff'
-            borderColor='transparent'
-            iconStyle={{ borderRadius: 5 }}
-            innerIconStyle={{ borderRadius: 5, backgroundColor: 'transparent' }}
-/>
-          <Text style={styles.heading}>Lowercase Letters</Text>
-        </View>
-        <View style={styles.inputWrapper}>
-          <BouncyCheckbox disableBuiltInState isChecked={upperCase} onPress={() => setUpperCase(!upperCase)} fillColor='#7676ff' borderColor='transparent'
-            iconStyle={{ borderRadius: 5 }}
-            innerIconStyle={{ borderRadius: 5, backgroundColor: 'transparent' }}/>
-          <Text style={styles.heading}>Uppercase Letters</Text>
-        </View>
-        <View style={styles.inputWrapper}>
-          <BouncyCheckbox disableBuiltInState isChecked={numbers} onPress={() => setNumbers(!numbers)} fillColor='#7676ff' borderColor='transparent'
-            iconStyle={{ borderRadius: 5 }}
-            innerIconStyle={{ borderRadius: 5, backgroundColor: 'transparent' }}/>
-          <Text style={styles.heading}>Numbers</Text>
-        </View>
-        <View style={styles.inputWrapper}>
-          <BouncyCheckbox disableBuiltInState isChecked={symbols} onPress={() => setSymbols(!symbols)} fillColor='#7676ff' borderColor='transparent'
-            iconStyle={{ borderRadius: 5 }}
-            innerIconStyle={{ borderRadius: 5, backgroundColor: 'transparent' }}/>
-          <Text style={styles.heading}>Symbols</Text>
-        </View>
-        <View style={styles.formActions}>
-          <TouchableOpacity disabled={!isValid} style={styles.primaryBtn} onPress={handleSubmit}>
-            <Text style={styles.primaryBtnText}>Generate</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.secondaryBtn} onPress={() => {
-            handleReset()
-            resetPasswordState()
-          }}>
-            <Text style={styles.secondaryBtnText}>Reset</Text>
-          </TouchableOpacity>
-        </View>
-        </>
-      )}
+          >
+            {({
+              values,
+              isValid,
+              handleChange,
+              handleSubmit,
+              handleReset
+            }) => (
+              <>
+              <View style={styles.sliderValueContainer}>
+                  <Text style={styles.sliderValueText}>Length</Text>
+                  <Text style={styles.sliderValueText}>{values.passwordLength}</Text>
+                </View>
+                <View style={styles.slider}>
+                <Slider
+                  style={{ width: 300, height: 40}}
+                  step={1}
+                  minimumValue={4}
+                  maximumValue={16}
+                  value={values.passwordLength}
+                  onValueChange={(value) => {
+                    handleChange("passwordLength")(String(value));
+                  }}
+                  minimumTrackTintColor='#7676ff'
+                />
+              </View>
+              <View style={styles.inputWrapper}>
+                <BouncyCheckbox 
+                  disableBuiltInState 
+                  isChecked={lowerCase} 
+                  onPress={() => setLowerCase(!lowerCase)} 
+                  fillColor='#7676ff'
+                  borderColor='transparent'
+                  iconStyle={{ borderRadius: 5 }}
+                  innerIconStyle={{ borderRadius: 5, backgroundColor: 'transparent' }}
+                />
+                <Text style={styles.heading}>Lowercase Letters</Text>
+              </View>
+              <View style={styles.inputWrapper}>
+                <BouncyCheckbox disableBuiltInState isChecked={upperCase} onPress={() => setUpperCase(!upperCase)} fillColor='#7676ff' borderColor='transparent'
+                  iconStyle={{ borderRadius: 5 }}
+                  innerIconStyle={{ borderRadius: 5, backgroundColor: 'transparent' }}/>
+                <Text style={styles.heading}>Uppercase Letters</Text>
+              </View>
+              <View style={styles.inputWrapper}>
+                <BouncyCheckbox disableBuiltInState isChecked={numbers} onPress={() => setNumbers(!numbers)} fillColor='#7676ff' borderColor='transparent'
+                  iconStyle={{ borderRadius: 5 }}
+                  innerIconStyle={{ borderRadius: 5, backgroundColor: 'transparent' }}/>
+                <Text style={styles.heading}>Numbers</Text>
+              </View>
+              <View style={styles.inputWrapper}>
+                <BouncyCheckbox disableBuiltInState isChecked={symbols} onPress={() => setSymbols(!symbols)} fillColor='#7676ff' borderColor='transparent'
+                  iconStyle={{ borderRadius: 5 }}
+                  innerIconStyle={{ borderRadius: 5, backgroundColor: 'transparent' }}/>
+                <Text style={styles.heading}>Symbols</Text>
+              </View>
+              <View style={styles.formActions}>
+                <TouchableOpacity disabled={!isValid} style={styles.primaryBtn} onPress={handleSubmit}>
+                  <Text style={styles.primaryBtnText}>Generate</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.secondaryBtn} onPress={() => {
+                  handleReset()
+                  resetPasswordState()
+                }}>
+                  <Image source={require('./assets/undo.png')} style={{height: 40, width: 40}}/>
+                </TouchableOpacity>
+              </View>
+              </>
+            )}
           </Formik>
         </View>
       </SafeAreaView>
@@ -167,7 +166,7 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {flex: 1, flexDirection: "column", marginTop: 75},
   infoContainer: {alignItems: "center"},
-  title: {fontSize: 32, fontWeight: "bold" },
+  title: {fontSize: 32, fontWeight: "bold", textShadowColor: '#d8d8ff', textShadowOffset: { width: 2, height: 2 }, textShadowRadius: 2, },
   image: {height: 100, width: 100, marginTop: 40, marginBottom: 30},
   passwordBox: {marginBottom: 40, borderColor: "#EEEEEE", borderWidth: 2, width: 300, height: 60, borderRadius: 10, alignItems: "center", justifyContent: "center"},
   formContainer: {flex: 1,},
@@ -181,5 +180,4 @@ const styles = StyleSheet.create({
   primaryBtnText: {color: "white", fontWeight:"bold"},
   secondaryBtn: {backgroundColor: "#d8d8ff", width: 100, height: 70, borderRadius: 15, justifyContent: "center", alignItems: "center"},
   secondaryBtnText: {}
-
 });
